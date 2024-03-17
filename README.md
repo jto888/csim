c2f57bb500702e710d5ca9d2e0bd4f3cbc6b2929
Jacob Ormerod <jto888@users.noreply.github.com>
2024-03-16 22:29:51 -0400

    initial blank load

---
+++ README.md
@@ -0,0 +1,6 @@
+csim
+
+ 
+
+A small development package for writing the internals of the StochasticChain
+solution for the CARMS package using Rcpp.
---
+++ csim/src/csim.h
@@ -0,0 +1,16 @@
+#ifndef _csim_H
+#define _csim_H
+
+#include <RcppArmadillo.h>
+//#include <Rcpp.h>
+#include <vector>
+
+
+
+RcppExport SEXP Basicsim( SEXP, SEXP, SEXP, SEXP);
+
+
+
+		
+#endif
+
