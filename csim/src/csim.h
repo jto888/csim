21d08b0b0f487f5900497b9faa554d3e11300ddc
Jacob Ormerod <jto888@users.noreply.github.com>
2024-03-20 19:24:03 -0400

    Simulation class created

--- csim/src/csim.h
+++ csim/src/csim.h
@@ -4,9 +4,12 @@
 #include <RcppArmadillo.h>
 //#include <Rcpp.h>
 #include <vector>
+#include <memory>
 
+#include "Simulation.h"
 
 RcppExport SEXP ChainSim( SEXP, SEXP, SEXP, SEXP);
+RcppExport SEXP ChainSim2( SEXP, SEXP, SEXP, SEXP);
 
 Rcpp::IntegerVector which(Rcpp::IntegerVector vecDup, int elem);
 		
